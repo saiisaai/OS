0af82eb3543d420e599ae3ed5b4a0083bb5f2f59
chanakarn sriudom <88524659+saiisaai@users.noreply.github.com>
2023-08-20 15:01:43 +0700

    Update TimeingIO.py

--- TimeingIO.py
+++ TimeingIO.py
@@ -1,20 +1,16 @@
 import timeit
 import os
 
-# Prepare a large amount of data
-data = 'x' * 10**9  # 1GB of data
+data = 'x' * 10**9 
 
-# I/O-bound operation: Writing to a file
 start = timeit.default_timer()
 with open('OS_IOtime.txt', 'w') as file:
     file.write(data)
 end = timeit.default_timer()
 print(f"I/O time : {end - start} s")
 
-# Clean up the large file
 os.remove('OS_IOtime.txt')
 
-# CPU-bound operation: Calculating a large Fibonacci number
 fac = [0] * 1000
 def factorial(n):
     if (n == 0 or n == 1) : 
